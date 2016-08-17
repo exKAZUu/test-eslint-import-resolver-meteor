fcdd926c9b9686e8b2db6a3eb317ea2fc0961a20
Sakamoto, Kazunori <exkazuu@gmail.com>
2016-08-17 18:19:49 +0900

    Reproduce import problem on Windows.

---
+++ imports/server/value34.js
@@ -0,0 +1,2 @@
+export const value3 = 1;
+export const value4 = 2;
--- server/main.js
+++ server/main.js
@@ -1,5 +1,7 @@
 import { Meteor } from 'meteor/meteor';
+import { value1, value2 } from './values/value12';
+import { value3, value4 } from '../imports/server/value34';
 
 Meteor.startup(() => {
-  // code to run on server at startup
+  console.log(value1, value2, value3, value4);
 });
